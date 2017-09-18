7bbc8ea3717817ce320e46b28d4174751b57bec8
glevente <gergely.levente0@gmail.com>
2017-09-18 16:38:38 +0200

    fix filter callbacks

--- src/helpers.php
+++ src/helpers.php
@@ -1,17 +1,17 @@
 <?php
 
-if (!function_exists('sortArrayKeysToOrder')){
-    function sortArrayKeysToOrder($array, $keysOrder){
+if (!function_exists('sortArrayKeysToOrder')) {
+    function sortArrayKeysToOrder($array, $keysOrder)
+    {
         if (!is_array($array)) return $array;
         $keysOrder = array_flip($keysOrder);
-        $nonExistingItems = array_where($array, function($key, $value) use ($keysOrder){
+        $nonExistingItems = array_where($array, function ($value, $key) use ($keysOrder) {
             return !isset($keysOrder[$key]);
         });
-        $existingItems = array_where($array, function($key, $value) use ($keysOrder){
+        $existingItems = array_where($array, function ($value, $key) use ($keysOrder) {
             return isset($keysOrder[$key]);
         });
-
-        uksort($existingItems, function($a, $b) use ($keysOrder){
+        uksort($existingItems, function ($a, $b) use ($keysOrder) {
             return $keysOrder[$a] - $keysOrder[$b];
         });
 
