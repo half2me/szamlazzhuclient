5a6fd802ca7d0ae00b0fad419742150aae591d5d
glevente <gergely.levente0@gmail.com>
2017-09-26 18:32:03 +0200

    validator

--- src/Validator.php
+++ src/Validator.php
@@ -2,12 +2,15 @@
 
 namespace Clapp\SzamlazzhuClient;
 
-use Symfony\Component\Translation\Translator;
+use Illuminate\Translation\ArrayLoader;
+use Illuminate\Translation\Translator;
 use Illuminate\Validation\Validator as BaseValidator;
 
-class Validator extends BaseValidator{
+class Validator extends BaseValidator
+{
 
-    public static function make(array $data, array $rules, array $messages = [], array $customAttributes = []){
-        return new static(new Translator('en'), $data, $rules, $messages, $customAttributes);
+    public static function make(array $data, array $rules, array $messages = [], array $customAttributes = [])
+    {
+        return new static(new Translator(new ArrayLoader(), 'en'), $data, $rules, $messages, $customAttributes);
     }
 }
